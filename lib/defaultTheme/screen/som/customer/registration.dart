f53850a8770c5a0a5e59a56f4da4ea953a6391ab
slavisam <slavisam@gmail.com>
2021-08-05 17:24:23 +0200

    impl: CCustomerRegistration as host app for registration of customer

---
+++ lib/defaultTheme/screen/som/customer/registration.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CCustomerRegistration extends StatefulWidget {
+  @override
+  createState() => CCustomerRegistrationState();
+}
+
+class CCustomerRegistrationState extends State<CCustomerRegistration> {
+  Widget build(BuildContext context) {
+    return Text('mojga');
+  }
+}
