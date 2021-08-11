33cdad83e807c5fdf02d66d5df79358adf065859
slavisam <slavisam@gmail.com>
2021-08-11 09:51:25 +0200

    ref: selection variations for selection selectionCards and selectionLayoutBuilder widgets

--- lib/defaultTheme/screen/som/customer/RoleSelection.dart
+++ lib/defaultTheme/screen/som/customer/RoleSelection.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:som/main/utils/AppConstant.dart';
 import 'Role.dart';
 import 'Roles.dart';
 
@@ -14,47 +16,73 @@ class CRoleSelection extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('Please select role.', style: primaryTextStyle(size: 18))
-            .paddingOnly(left: 8, right: 8),
+        Text('Please select role.',
+                style: primaryTextStyle(size: textSizeXLarge.toInt()))
+            .paddingOnly(left: 8, top: 20, right: 8),
         16.height,
         Container(
           height: 150,
-          child: LayoutBuilder(
-            builder: (context, constraints) => ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20.0),
-                  constraints: BoxConstraints(
-                      minWidth: constraints.maxWidth / 4,
-                      minHeight: constraints.maxHeight),
-                  child: Center(
-                    child: Role(
-                      selectRole: selectRole,
-                      selectedRole: selectedRole,
-                      role: Roles.Provider,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(20.0),
-                  constraints: BoxConstraints(
-                      minWidth: constraints.maxWidth / 4,
-                      minHeight: constraints.maxHeight),
-                  child: Center(
-                    child: Role(
-                      selectRole: selectRole,
-                      selectedRole: selectedRole,
-                      role: Roles.Buyer,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+          child: selectionCards,
         ),
         8.height,
       ],
+    );
+  }
+
+  Widget get selectionCards {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: Roles.values
+          .map(
+            (roleValue) => InkWell(
+              child: Role(
+                selectRole: selectRole,
+                selectedRole: selectedRole,
+                role: roleValue,
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  // hack approach to container spacing to parent constraints
+  LayoutBuilder get selectionLayoutBuilder {
+    return LayoutBuilder(
+      builder: (context, constraints) => ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          // Provider selection
+          Container(
+            padding: EdgeInsets.all(20.0),
+            constraints: BoxConstraints(
+                // 1/4 is ratio of container view of max constraint
+                minWidth: constraints.maxWidth / 4,
+                minHeight: constraints.maxHeight),
+            child: Center(
+              child: Role(
+                selectRole: selectRole,
+                selectedRole: selectedRole,
+                role: Roles.Provider,
+              ),
+            ),
+          ),
+          // Buyer selection
+          Container(
+            padding: EdgeInsets.all(20.0),
+            constraints: BoxConstraints(
+                minWidth: constraints.maxWidth / 4,
+                minHeight: constraints.maxHeight),
+            child: Center(
+              child: Role(
+                selectRole: selectRole,
+                selectedRole: selectedRole,
+                role: Roles.Buyer,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
