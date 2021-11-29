92c154a837919090809d830cace76f4f3b43b4f4
slavisam <slavisam@gmail.com>
2021-11-29 12:04:19 +0100

    impl: reactivate user menu after merge

--- lib/defaultTheme/screen/app/DashboardScreen.dart
+++ lib/defaultTheme/screen/app/DashboardScreen.dart
@@ -33,24 +33,86 @@ class DashboardScreenState extends State<DashboardScreen> {
     if (mounted) super.setState(fn);
   }
 
-  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Observer(
-        builder: (context) => Scaffold(
-          appBar: AppBar(
-              backgroundColor: appStore.appBarColor,
-              title: appBarTitleWidget(context, 'Dashboard'),
-              iconTheme: IconThemeData(color: appStore.iconColor)),
-          floatingActionButton: userMenu(),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-          drawer: MainMenu(),
-          body: DTWorkInProgressScreen(),
-          // body: DTDashboardWidget(),
-        ),
-      ),
-    );
+    return appStore.isAuthenticated
+        ? SafeArea(
+            child: Observer(
+              builder: (context) => Scaffold(
+                appBar: AppBar(
+                  backgroundColor: appStore.appBarColor,
+                  title: appBarTitleWidget(context, 'Dashboard'),
+                  iconTheme: IconThemeData(color: appStore.iconColor),
+                  actions: [
+                    PopupMenuButton(
+                      child: Row(children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.grey.shade800,
+                          backgroundImage: AssetImage(
+                              'images/widgets/materialWidgets/mwInformationDisplayWidgets/gridview/ic_item4.jpg'),
+                        ).paddingRight(15.0),
+                        Text('Fritzchen der Käufer').paddingRight(50),
+                      ]),
+                      itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                        PopupMenuItem(
+                          child: ListTile(
+                            leading: Icon(Icons.notifications),
+                            title: Text('Notifications'),
+                          ),
+                        ),
+                        PopupMenuItem(
+                          child: ListTile(
+                            leading: Icon(Icons.manage_accounts),
+                            title: Text('User account'),
+                          ),
+                        ),
+                        PopupMenuItem(
+                          child: ListTile(
+                            leading: Icon(Icons.settings_applications),
+                            title: Text('App configuration'),
+                          ),
+                        ),
+                        PopupMenuDivider(),
+                        PopupMenuItem(
+                          child: ListTile(
+                            leading: Icon(Icons.logout),
+                            title: Text('Logout'),
+                            onTap: () {
+                              appStore.logout();
+                              LoginOrRegister().launch(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                drawer: MainMenu(),
+                body: DTWorkInProgressScreen(),
+                // body: DTDashboardWidget(),
+              ),
+            ),
+          )
+        : LoginOrRegister();
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return SafeArea(
+  //     child: Observer(
+  //       builder: (context) => Scaffold(
+  //         appBar: AppBar(
+  //             backgroundColor: appStore.appBarColor,
+  //             title: appBarTitleWidget(context, 'Dashboard'),
+  //             iconTheme: IconThemeData(color: appStore.iconColor)),
+  //         floatingActionButton: userMenu(),
+  //         floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+  //         drawer: MainMenu(),
+  //         body: DTWorkInProgressScreen(),
+  //         // body: DTDashboardWidget(),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   userMenu() {
     return LayoutBuilder(
@@ -87,111 +149,111 @@ class DashboardScreenState extends State<DashboardScreen> {
     );
   }
 }
-
-class ItemTile extends StatefulWidget {
-//   final OrderItem orderItem;
-
-//   OrderItemTile(this.title);
-
-  @override
-  _ItemTileState createState() => _ItemTileState();
-}
-
-class _ItemTileState extends State<ItemTile> {
-  var _expanded;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
-      height: _expanded ? 350 : 100,
-      child: Container(
-        width: 300,
-        child: Card(
-          elevation: 10,
-          color: Theme.of(context).canvasColor,
-          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                title: (Text(
-                  'File',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                )),
-                trailing: IconButton(
-                    icon: _expanded
-                        ? Icon(Icons.expand_less)
-                        : Icon(Icons.expand_more),
-                    onPressed: () {
-                      setState(() {
-                        _expanded = !_expanded;
-                      });
-                    }),
-              ),
-              AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                height: _expanded ? 300 : 0,
-                width: MediaQuery.of(context).size.width,
-                child: ItemExpandedTile(),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ItemExpandedTile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 50,
-            child: Container(
-              height: 90,
-              width: 200,
-              padding: EdgeInsets.all(10),
-              decoration: new BoxDecoration(
-                color: Theme.of(context).canvasColor,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 15.0,
-                    spreadRadius: 0.5,
-                    offset: Offset(
-                      1.0,
-                      1.0,
-                    ),
-                  )
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text(
-                        'Title',
-                        style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//
+// class ItemTile extends StatefulWidget {
+// //   final OrderItem orderItem;
+//
+// //   OrderItemTile(this.title);
+//
+//   @override
+//   _ItemTileState createState() => _ItemTileState();
+// }
+//
+// class _ItemTileState extends State<ItemTile> {
+//   var _expanded;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedContainer(
+//       duration: Duration(milliseconds: 300),
+//       height: _expanded ? 350 : 100,
+//       child: Container(
+//         width: 300,
+//         child: Card(
+//           elevation: 10,
+//           color: Theme.of(context).canvasColor,
+//           margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+//           child: Column(
+//             children: <Widget>[
+//               ListTile(
+//                 title: (Text(
+//                   'File',
+//                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+//                 )),
+//                 trailing: IconButton(
+//                     icon: _expanded
+//                         ? Icon(Icons.expand_less)
+//                         : Icon(Icons.expand_more),
+//                     onPressed: () {
+//                       setState(() {
+//                         _expanded = !_expanded;
+//                       });
+//                     }),
+//               ),
+//               AnimatedContainer(
+//                 duration: Duration(milliseconds: 300),
+//                 height: _expanded ? 300 : 0,
+//                 width: MediaQuery.of(context).size.width,
+//                 child: ItemExpandedTile(),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class ItemExpandedTile extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+//       child: Stack(
+//         children: <Widget>[
+//           Positioned(
+//             top: 50,
+//             child: Container(
+//               height: 90,
+//               width: 200,
+//               padding: EdgeInsets.all(10),
+//               decoration: new BoxDecoration(
+//                 color: Theme.of(context).canvasColor,
+//                 borderRadius: BorderRadius.circular(15),
+//                 boxShadow: [
+//                   BoxShadow(
+//                     color: Colors.grey,
+//                     blurRadius: 15.0,
+//                     spreadRadius: 0.5,
+//                     offset: Offset(
+//                       1.0,
+//                       1.0,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//               child: Row(
+//                 children: <Widget>[
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                     children: <Widget>[
+//                       Text(
+//                         'Title',
+//                         style: TextStyle(
+//                             fontSize: 12, fontWeight: FontWeight.bold),
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 /*
   Widget build(BuildContext context) {
