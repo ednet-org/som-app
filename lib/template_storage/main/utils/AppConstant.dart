062b96cd6df04bc3fee9d82e3dee137653d502ef
slavisam <slavisam@gmail.com>
2022-01-25 18:10:36 +0100

    init stepper

--- lib/template_storage/main/utils/AppConstant.dart
+++ lib/template_storage/main/utils/AppConstant.dart
@@ -19,23 +19,28 @@ const textSizeXXLarge = 35.0;
 const appOpenCount = 'appOpenCount';
 
 /// Linux - for linux, you have to change default window width in linux/my_application.cc
-const applicationMaxWidth = 500.0;
+const applicationMaxWidth = 300.0;
 
-const profileImage = 'images/widgets/materialWidgets/mwInputSelectionWidgets/Checkbox/profile.png';
+const profileImage =
+    'images/widgets/materialWidgets/mwInputSelectionWidgets/Checkbox/profile.png';
 const isDarkModeOnPref = 'isDarkModeOnPref';
 const dateFormat = 'MMM dd, yyyy';
 
 const BaseUrl = 'https://smart-offer-management.web.app';
-const SourceCodeUrl = 'https://codecanyon.net/item/prokit-flutter-app-ui-design-templete-kit/25787190?s_rank=11';
+const SourceCodeUrl =
+    'https://codecanyon.net/item/prokit-flutter-app-ui-design-templete-kit/25787190?s_rank=11';
 const PlayStoreUrl = 'https://play.google.com/store/apps/details?id=';
 const DocumentationUrl = 'https://iqonic.design/docs/product/prokit-flutter/';
-const ChangeLogsUrl = 'https://iqonic.design/docs/product/prokit-flutter/updates/change-logs/';
-const MimikCloneUrl = 'https://codecanyon.net/item/mimik-multi-category-flutter-app-ui-kit-clone/29382568?s_rank=2';
+const ChangeLogsUrl =
+    'https://iqonic.design/docs/product/prokit-flutter/updates/change-logs/';
+const MimikCloneUrl =
+    'https://codecanyon.net/item/mimik-multi-category-flutter-app-ui-kit-clone/29382568?s_rank=2';
 
 const bannerAdIdForAndroidRelease = "ca-app-pub-1399327544318575/5026584528";
 const bannerAdIdForAndroid = "ca-app-pub-3940256099942544/6300978111";
 const bannerAdIdForIos = "ca-app-pub-3940256099942544/2934735716";
-const InterstitialAdIdForAndroidRelease = "ca-app-pub-1399327544318575/8774597158";
+const InterstitialAdIdForAndroidRelease =
+    "ca-app-pub-1399327544318575/8774597158";
 const InterstitialAdIdForAndroid = "ca-app-pub-3940256099942544/1033173712";
 const interstitialAdIdForIos = "ca-app-pub-3940256099942544/4411468910";
 
@@ -43,7 +48,8 @@ const SampleImageUrl = '$BaseUrl/images/template_storage/slider/01.jpg';
 const SampleImageUrl2 = '$BaseUrl/images/template_storage/slider/04.jpg';
 const SampleImageUrl3 = '$BaseUrl/images/template_storage/slider/03.jpg';
 const SampleImageUrl4 = '$BaseUrl/images/template_storage/slider/05.jpg';
-const SampleProfileImageUrl = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200';
+const SampleProfileImageUrl =
+    'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200';
 
 const ProductImg1 = '$BaseUrl/images/template_storage/product/Bag.png';
 const ProductImg2 = '$BaseUrl/images/template_storage/product/Chair.png';
@@ -55,7 +61,8 @@ const ProductImg7 = '$BaseUrl/images/template_storage/product/shoes.png';
 const ProductImg8 = '$BaseUrl/images/template_storage/product/skincare.png';
 const ProductImg9 = '$BaseUrl/images/template_storage/product/tshirt.png';
 
-const LoremText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.This is simply text';
+const LoremText =
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.This is simply text';
 
 // App Names
 const mpAppName = 'Music PodCast';
--- lib/ui/pages/CustomerRegistrationPage.dart
+++ lib/ui/pages/CustomerRegistrationPage.dart
@@ -1,45 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:som/main.dart';
 import 'package:som/template_storage/main/utils/AppColors.dart';
+import 'package:som/template_storage/main/utils/AppWidget.dart';
 import 'package:som/template_storage/screen/template/DTSignInScreen.dart';
+import 'package:som/ui/components/layout/MainBody.dart';
 import 'package:som/ui/pages/customer/registration/Registration.dart';
 
-class CustomerRegistrationPage extends StatelessWidget {
+class CustomerRegistrationPage extends StatefulWidget {
   static String tag = '/CustomerRegistrationPage';
 
   @override
+  State<CustomerRegistrationPage> createState() =>
+      _CustomerRegistrationPageState();
+}
+
+class _CustomerRegistrationPageState extends State<CustomerRegistrationPage> {
+  int currStep = 1;
+
+  @override
   Widget build(BuildContext context) {
+    List<Step> mSteps = [
+      Step(
+          title: Text("Role selection"),
+          content: Text("ROLE",
+              style: secondaryTextStyle(color: appStore.textSecondaryColor)),
+          isActive: currStep == 0,
+          state: StepState.complete),
+      Step(
+          title: Text("Company details"),
+          content: Text("COMPANY",
+              style: secondaryTextStyle(color: appStore.textSecondaryColor)),
+          isActive: currStep == 1,
+          state: StepState.disabled),
+      Step(
+          title: Text("Subscription model"),
+          content: Text("SUBSCRIPTION",
+              style: secondaryTextStyle(color: appStore.textSecondaryColor)),
+          isActive: currStep == 2,
+          state: StepState.disabled),
+      Step(
+          title: Text("Bank details"),
+          content: Text("BANK",
+              style: secondaryTextStyle(color: appStore.textSecondaryColor)),
+          isActive: currStep == 3,
+          state: StepState.disabled),
+      Step(
+          title: Text("USER"),
+          content: Text("USER",
+              style: secondaryTextStyle(color: appStore.textSecondaryColor)),
+          isActive: currStep == 4,
+          state: StepState.disabled),
+    ];
+
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Registration(),
-            20.height,
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-              decoration: BoxDecoration(
-                  color: appColorPrimary,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: defaultBoxShadow()),
-              child: Text('Register',
-                  style: boldTextStyle(color: white, size: 18)),
-            ).onTap(() {
-              finish(context);
-            }),
-            20.height,
-            Text('Already Registered?',
-                style: boldTextStyle(
-                  color: appColorPrimary,
-                )).center().onTap(() {
-              DTSignInScreen().launch(context);
-            }),
-          ],
-        ),
-      ).center(),
+      body: MainBody(
+        child: Stepper(
+          steps: mSteps,
+          type: StepperType.horizontal,
+          currentStep: this.currStep,
+          controlsBuilder: (BuildContext context, ControlsDetails details) {
+            return Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                TextButton(
+                  onPressed: details.onStepContinue,
+                  child: Text('CONTINUE', style: secondaryTextStyle()),
+                ),
+                10.width,
+                TextButton(
+                  onPressed: details.onStepCancel,
+                  child: Text('CANCEL', style: secondaryTextStyle()),
+                ),
+              ],
+            );
+          },
+          onStepContinue: () {
+            setState(() {
+              if (currStep < mSteps.length - 1) {
+                currStep = currStep + 1;
+              } else {
+                //currStep = 0;
+                finish(context);
+              }
+            });
+          },
+          onStepCancel: () {
+            finish(context);
+            setState(() {
+              if (currStep > 0) {
+                currStep = currStep - 1;
+              } else {
+                currStep = 0;
+              }
+            });
+          },
+          onStepTapped: (step) {
+            setState(() {
+              currStep = step;
+            });
+          },
+        ).center(),
+      ),
+    );
+  }
+
+  previousSolution(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Registration(),
+        20.height,
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          decoration: BoxDecoration(
+              color: appColorPrimary,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: defaultBoxShadow()),
+          child: Text('Register man',
+              style: boldTextStyle(color: white, size: 18)),
+        ).onTap(() {
+          finish(context);
+        }),
+        20.height,
+        Text('Already Registered?',
+            style: boldTextStyle(
+              color: appColorPrimary,
+            )).center().onTap(() {
+          DTSignInScreen().launch(context);
+        }),
+      ],
     );
   }
 }
