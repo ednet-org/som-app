04e19cbbe7e2f9ee34b3fd3ff1eff90121d46bc9
slavisam <slavisam@gmail.com>
2022-06-02 15:19:05 +0200

    impl: full user info on customer registration

--- lib/domain/model/customer-management/registration_user.dart
+++ lib/domain/model/customer-management/registration_user.dart
@@ -1,5 +1,4 @@
 import 'package:mobx/mobx.dart';
-import 'package:som/domain/model/customer-management/company.dart';
 
 part 'registration_user.g.dart';
 
@@ -7,16 +6,60 @@ part 'registration_user.g.dart';
 /// it can have state - be invited and prepopulated
 class RegistrationUser = _RegistrationUser with _$RegistrationUser;
 
+enum CompanyRole { admin, employee }
+
 abstract class _RegistrationUser with Store {
+  @observable
+  String? firstName;
+
+  @observable
+  String? lastName;
+
   @observable
   String? email;
 
-  @action
-  void setEmail(String value) => salutation = value;
+  @observable
+  String? title;
+
+  @observable
+  String? phone;
 
   @observable
   String? salutation;
 
   @action
   void setSalutation(String value) => salutation = value;
+
+  @observable
+  String? terms;
+
+  @observable
+  String? policies;
+
+  @action
+  void setPolicies(String value) => policies = value;
+
+  @action
+  void setTerms(String value) => terms = value;
+
+  @observable
+  CompanyRole role = CompanyRole.employee;
+
+  @action
+  void setFirstName(String value) => firstName = value;
+
+  @action
+  void setLastName(String value) => lastName = value;
+
+  @action
+  void setEmail(String value) => email = value;
+
+  @action
+  void setPhone(String value) => phone = value;
+
+  @action
+  void setTitle(String value) => title = value;
+
+  @computed
+  get hasAcceptedTermsAndPolicies => terms != null && policies != null;
 }
