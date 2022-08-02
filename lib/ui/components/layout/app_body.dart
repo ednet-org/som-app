4acc24c96906f907f6c7d4ddcac259ac2795d3f0
slavisam <slavisam@gmail.com>
2022-08-02 13:13:52 +0200

    wip

--- lib/ui/components/layout/app_body.dart
+++ lib/ui/components/layout/app_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:som/ui/components/cards/inquiry_info_card.dart';
 
 class AppBody extends StatelessWidget {
   const AppBody({
@@ -88,21 +89,180 @@ class AppBody extends StatelessWidget {
     );
   }
 
-  Column get overdueInquiries {
-    return Column(
-      children: [
-        Text("1"),
-        Text("2"),
-        Text("3"),
+  CustomScrollView get overdueInquiries {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverToBoxAdapter(
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            color: Colors.yellow,
+          ),
+        ),
+        SliverPadding(padding: EdgeInsets.only(top: 10)),
+        SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0),
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return BodyWidget();
+            },
+            childCount: 30,
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.only(bottom: 80.0),
+        )
       ],
     );
   }
 
-  Column get currentInquiries {
-    return Column(
-      children: [Text("1"), Text("2")],
-    );
+  //
+  // Column get overdueInquiries {
+  //   return Column(
+  //     children: [
+  //       _buildCard("1"),
+  //       _buildCard("2"),
+  //       _buildCard("3"),
+  //       _buildCard("4"),
+  //       _buildCard("5"),
+  //       _buildCard("6"),
+  //       _buildCard("7"),
+  //       _buildCard("5"),
+  //       _buildCard("5"),
+  //       _buildCard("5"),
+  //     ],
+  //   );
+  // }
+
+  _buildCard(String value) {
+    return Expanded(
+        child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 20.0),
+            width: 300,
+            height: 400,
+            child: Card(
+              child: Expanded(
+                  child: Text(value,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30))),
+            )));
+  }
+
+  TestHorizontalListView get currentInquiries {
+    return TestHorizontalListView();
   }
 
   Container get contextMenu => Container(child: SizedBox());
+}
+
+class TestHorizontalListView extends StatefulWidget {
+  TestHorizontalListView({Key? key}) : super(key: key);
+
+  @override
+  State<TestHorizontalListView> createState() => _TestHorizontalListViewState();
+}
+
+class _TestHorizontalListViewState extends State<TestHorizontalListView> {
+  List<String> lstData = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'P',
+    'U',
+    'V',
+    'W'
+  ];
+
+  final ScrollController _horizontal_scrollcontroller = ScrollController();
+
+  _buildCard(String value) {
+    return Expanded(
+        child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 20.0),
+            width: 300,
+            height: 400,
+            child: Card(
+              child: Expanded(
+                  child: Text(value,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30))),
+            )));
+  }
+
+  void _scrollRight() {
+    _horizontal_scrollcontroller.animateTo(
+      _horizontal_scrollcontroller.position.maxScrollExtent,
+      duration: Duration(seconds: 1),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void _scrollLeft() {
+    _horizontal_scrollcontroller.animateTo(
+      0,
+      duration: Duration(seconds: 1),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  @override
+  void initState() {
+    //   TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 1,
+        child: SingleChildScrollView(
+            child: Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width,
+                child: Row(children: [
+                  FloatingActionButton.small(
+                    onPressed: _scrollRight,
+                    child: const Icon(Icons.arrow_right),
+                  ),
+                  Expanded(
+                    child: Scrollbar(
+                      child: ListView.builder(
+                          itemCount: lstData.length,
+                          controller: _horizontal_scrollcontroller,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 20.0),
+                                width: 100,
+                                height: 100,
+                                child: Card(
+                                  child: Text(lstData[index],
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(fontSize: 30)),
+                                ));
+                          }),
+                    ),
+                  ),
+                  FloatingActionButton.small(
+                    onPressed: _scrollLeft,
+                    child: const Icon(Icons.arrow_left),
+                  ),
+                ]))));
+  }
 }
