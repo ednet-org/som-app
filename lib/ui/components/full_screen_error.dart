5714f3262df4a54564427c24cdee6481aac71377
slavisam <slavisam@gmail.com>
2022-08-29 20:51:22 +0200

    Fix

--- lib/ui/components/full_screen_error.dart
+++ lib/ui/components/full_screen_error.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class FullScreenError extends StatelessWidget {
   const FullScreenError({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return showMaterialModalBottomSheet(
-        context: context, builder: (builderContext) {
-      return Container();
-    })
+    return Container();
   }
 }
