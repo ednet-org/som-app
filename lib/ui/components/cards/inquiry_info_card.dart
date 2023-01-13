c862ff4a885955342a180727ef85b0fb02c85812
slavisam <slavisam@gmail.com>
2023-01-13 16:01:11 +0100

    Refactor InquiryInfoCard to be more compact

--- lib/ui/components/cards/inquiry_info_card.dart
+++ lib/ui/components/cards/inquiry_info_card.dart
@@ -1,20 +1,114 @@
 import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 
-class InquiryInfoCard extends StatefulWidget {
+class PositionedInfo extends StatelessWidget {
+  final double top;
+  final double left;
+  final Widget child;
+
+  const PositionedInfo(
+      {super.key, required this.top, required this.left, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: top,
+      left: left,
+      child: child,
+    );
+  }
+}
+
+class InquiryInfoCard extends StatelessWidget {
   final Map<String, Object> inquiry;
 
   const InquiryInfoCard({super.key, required this.inquiry});
 
   @override
-  InquiryInfoCardState createState() => InquiryInfoCardState();
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 306.00164794921875,
+        height: 357,
+        child: Stack(children: <Widget>[
+          PositionedInfo(
+              top: 0,
+              left: 0,
+              child: _InquiryInfoCardContainer(inquiry: inquiry)),
+          _InquiryInfoCardTitle(inquiry: inquiry),
+          _InquiryInfoCardDescription(inquiry: inquiry),
+          _InquiryInfoCardStatus(inquiry: inquiry),
+          _InquiryInfoCardDivider(),
+        ]));
+  }
 }
 
-class InquiryInfoCardState extends State<InquiryInfoCard> {
-  // generate inquiry status color based on inquiry status
+class _InquiryInfoCardContainer extends StatelessWidget {
+  final Map<String, Object> inquiry;
+
+  const _InquiryInfoCardContainer({required this.inquiry});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 305,
+        height: 357,
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.15),
+                  offset: Offset(0, 2),
+                  blurRadius: 6)
+            ],
+            color: Color.fromRGBO(255, 251, 255, 1)));
+  }
+}
+
+class _InquiryInfoCardTitle extends StatelessWidget {
+  final Map<String, Object> inquiry;
+
+  const _InquiryInfoCardTitle({required this.inquiry});
+
+  @override
+  Widget build(BuildContext context) {
+    return const PositionedInfo(
+        top: 12,
+        left: 20,
+        child: Text(
+          'Inquiry',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              color: Color.fromRGBO(56, 30, 114, 1),
+              fontFamily: 'Work Sans',
+              fontSize: 24,
+              fontWeight: FontWeight.normal,
+              height: 1),
+        ));
+  }
+}
+
+class _InquiryInfoCardDescription extends StatelessWidget {
+  final Map<String, Object> inquiry;
+
+  const _InquiryInfoCardDescription({required this.inquiry});
+
+  @override
+  Widget build(BuildContext context) {
+    return PositionedInfo(
+      top: 15,
+      left: 108,
+      child: Text(inquiry['description'].toString()),
+    );
+  }
+}
+
+class _InquiryInfoCardStatus extends StatelessWidget {
+  final Map<String, Object> inquiry;
+
+  const _InquiryInfoCardStatus({required this.inquiry});
+
   Color get inquiryStatusColor {
-    var inquiryStatus = widget.inquiry['status'];
+    var inquiryStatus = inquiry['status'];
     switch (inquiryStatus) {
       case 'closed':
         return Colors.green;
@@ -31,191 +125,28 @@ class InquiryInfoCardState extends State<InquiryInfoCard> {
 
   @override
   Widget build(BuildContext context) {
-    // Figma Flutter Generator BodyWidget - GROUP
+    return PositionedInfo(
+        top: 20.5,
+        left: 270,
+        child: Container(
+            width: 17,
+            height: 15,
+            decoration: BoxDecoration(
+              color: inquiryStatusColor,
+              borderRadius: const BorderRadius.all(Radius.elliptical(17, 15)),
+            )));
+  }
+}
 
-    return SizedBox(
-        width: 306.00164794921875,
-        height: 357,
-        child: Stack(children: <Widget>[
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Container(
-                  width: 305,
-                  height: 357,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                      bottomLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.15000000596046448),
-                          offset: Offset(0, 2),
-                          blurRadius: 6)
-                    ],
-                    color: Color.fromRGBO(255, 251, 255, 1),
-                  ))),
-          const Positioned(
-              top: 12,
-              left: 20,
-              child: Text(
-                'Inquiry',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(56, 30, 114, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 24,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(
-              top: 15,
-              left: 108,
-              child: Text(
-                '1',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(56, 30, 114, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 24,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(top: 16, left: 251, child: SizedBox()),
-          Positioned(
-              top: 20.5,
-              left: 270,
-              child: Container(
-                  width: 17,
-                  height: 15,
-                  decoration: BoxDecoration(
-                    color: inquiryStatusColor,
-                    borderRadius:
-                        const BorderRadius.all(Radius.elliptical(17, 15)),
-                  ))),
-          Positioned(
-              top: 51,
-              left: 1,
-              child: Transform.rotate(
-                angle: 2.4848083448933725e-17 * (math.pi / 180),
-                child: const Divider(
-                    color: Color.fromRGBO(0, 0, 0, 1), thickness: 1),
-              )),
-          const Positioned(
-              top: 67,
-              left: 20,
-              child: Text(
-                'Description',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(98, 91, 113, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 16,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(
-              top: 113,
-              left: 20,
-              width: 280,
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-                textAlign: TextAlign.left,
-                softWrap: true,
-                style: TextStyle(
-                    color: Color.fromRGBO(98, 91, 113, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 12,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(
-              top: 230,
-              left: 227,
-              child: Text(
-                'Laptop',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(98, 91, 113, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 16,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(
-              top: 230,
-              left: 22,
-              child: Text(
-                'IT',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(98, 91, 113, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 16,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(top: 278, left: 20, child: SizedBox()),
-          const Positioned(
-              top: 279,
-              left: 52,
-              child: Text(
-                'Max Mustermann',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(28, 27, 30, 1),
-                    fontFamily: 'Work Sans',
-                    fontSize: 16,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-          const Positioned(top: 326, left: 11, child: SizedBox()),
-          const Positioned(top: 326, left: 179, child: SizedBox()),
-          const Positioned(
-              top: 327,
-              left: 39,
-              child: Text(
-                '10.09.2022',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(0, 0, 0, 1),
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    letterSpacing: 0.25,
-                    fontWeight: FontWeight.normal,
-                    height: 1.4285714285714286),
-              )),
-          const Positioned(
-              top: 327,
-              left: 204,
-              child: Text(
-                '10.11.2022',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    color: Color.fromRGBO(0, 0, 0, 1),
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    letterSpacing: 0.25,
-                    fontWeight: FontWeight.normal,
-                    height: 1.4285714285714286),
-              )),
-        ]));
+class _InquiryInfoCardDivider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PositionedInfo(
+        top: 51,
+        left: 1,
+        child: Transform.rotate(
+          angle: 2.4848083448933725e-17 * (math.pi / 180),
+          child: const Divider(color: Color.fromRGBO(0, 0, 0, 1), thickness: 1),
+        ));
   }
 }
