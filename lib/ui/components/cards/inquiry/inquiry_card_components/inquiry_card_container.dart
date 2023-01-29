c53383e137be9c08c14808abbb1d357cf4bc27d8
slavisam <slavisam@gmail.com>
2023-01-29 11:15:36 +0100

    Elevate InquiryCard by 5

--- lib/ui/components/cards/inquiry/inquiry_card_components/inquiry_card_container.dart
+++ lib/ui/components/cards/inquiry/inquiry_card_components/inquiry_card_container.dart
@@ -7,6 +7,7 @@ class InquiryCardContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: Card(
+        elevation: 5,
         child: SizedBox(
           width: 400,
           height: 375,
